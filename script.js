e79c3f76a9882c387875b5aae429ce6010d6946c
hxdith <hadithmukrsih21@gmail.com>
2023-02-07 22:08:33 +0800

    added game slideshow

--- script.js
+++ script.js
@@ -2,27 +2,29 @@ const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
-if (bar) {
-    bar.addEventListener('click', ()=> {
-        nav.classList.remove('active');
-    })
-}
 
-if (close) {
-    close.addEventListener('click', ()=> {
-        nav.classList.add('active');
-    })
-}
+	const toggleButton = document.getElementsByClassName('toggle-button')[0]
+	const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+		
+	toggleButton.addEventListener('click', () => {
+		navbarLinks.classList.toggle('active')
+		if(navbarLinks.style.display === 'none')
+			navbarLinks.style.display = 'block';
+		else
+			navbarLinks.style.display = 'none';
+	});
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'cb9f5f458fmsh642e483c83a8823p120ff7jsn546df944fb80',
-		'X-RapidAPI-Host': 'call-of-duty-modern-warfare.p.rapidapi.com'
-	}
-};
 
-fetch('https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/Amartin743/psn', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+
+
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'cb9f5f458fmsh642e483c83a8823p120ff7jsn546df944fb80',
+			'X-RapidAPI-Host': 'call-of-duty-modern-warfare.p.rapidapi.com'
+		}
+	};
+	fetch('https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/Amartin743/psn', options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));

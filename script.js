aa965ce8ab99ab2c07ec81a16ab6aa972be87477
hxdith <hadithmukrsih21@gmail.com>
2023-02-10 01:31:06 +0800

    fixed spin the wheel game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
-
+const navLinks = document.querySelectorAll('#navbar a');
 
 	const toggleButton = document.getElementsByClassName('toggle-button')[0]
 	const navbarLinks = document.getElementsByClassName('navbar-links')[0]
